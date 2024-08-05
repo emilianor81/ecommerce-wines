e2102d5046ed032f9040882a24a670cd3a496b8e
Emiliano Rodriguez <emilianor81@gmail.com>
2024-08-05 13:05:18 +0200

    Save work in progress

--- api/src/db.js
+++ api/src/db.js
@@ -3,12 +3,9 @@ const { DataTypes } = require('sequelize');
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
-  
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce`, {
-  logging: false, // set to console.log to see the raw SQL queries
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  logging: console.log, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
